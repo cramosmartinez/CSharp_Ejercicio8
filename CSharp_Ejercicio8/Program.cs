0b4f0eba9e837f44f7b0dbc86f4bdc5af53fa025
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 17:50:43 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Ejercicio8/Program.cs
@@ -0,0 +1,8 @@
+﻿//Recorre los números del 1 al 100. Muestra los números pares o divisibles entre 3
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 2 == 0 || i % 3 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
